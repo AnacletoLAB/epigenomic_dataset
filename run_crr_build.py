04c04422680188fb87c8c9c752ce88a6f95a56cd
LucaCappelletti94 <cappelletti.luca94@gmail.com>
2020-03-03 08:59:02 +0100

    Updated run to download also ROADMAP-related epigenomes

--- run_crr_build.py
+++ run_crr_build.py
@@ -1,4 +1,4 @@
-from crr_labels import fantom
+from crr_labels import fantom, roadmap
 from epigenomic_dataset import build
 from notipy_me import Notipy
 
@@ -9,40 +9,86 @@ with Notipy():
     cell_lines_fantom = cell_lines + ["MCF7"]
     windows_size = 1000
 
-    print("Downloading labels from FANTOM")
-    enhancers, promoters = fantom(
-        cell_lines=cell_lines_fantom, # list of cell lines to be considered.
-        window_size=windows_size, # window size to use for the various regions.
-        genome = "hg19", # considered genome version. Currently supported only "hg19".
-        center_enhancers = "peak", # how to center the enhancer window, either around "peak" or the "center" of the region.
-        enhancers_threshold = 0, # activation threshold for the enhancers.
-        promoters_threshold = 5, # activation threshold for the promoters.
-        drop_always_inactive_rows = False, # whetever to drop the rows where no activation is detected for every rows.
-        binarize = True, # Whetever to return the data binary-encoded, zero for inactive, one for active.
-        nrows = None # the number of rows to read, usefull when testing pipelines for creating smaller datasets.
+    # print("Downloading labels from FANTOM")
+    # enhancers, promoters = fantom(
+    #     cell_lines=cell_lines_fantom, # list of cell lines to be considered.
+    #     window_size=windows_size, # window size to use for the various regions.
+    #     genome = "hg19", # considered genome version. Currently supported only "hg19".
+    #     center_enhancers = "peak", # how to center the enhancer window, either around "peak" or the "center" of the region.
+    #     enhancers_threshold = 0, # activation threshold for the enhancers.
+    #     promoters_threshold = 5, # activation threshold for the promoters.
+    #     drop_always_inactive_rows = False, # whetever to drop the rows where no activation is detected for every rows.
+    #     binarize = True, # Whetever to return the data binary-encoded, zero for inactive, one for active.
+    #     nrows = None # the number of rows to read, usefull when testing pipelines for creating smaller datasets.
+    # )
+
+    # enhancers.to_csv("fantom_enhancers.bed", sep="\t")
+    # promoters.to_csv("fantom_promoters.bed", sep="\t")
+
+    # enhancers[["chrom","chromStart","chromEnd"]].to_csv(
+    #     "enhancers_regions.bed",
+    #     sep="\t",
+    #     header=False,
+    #     index=False
+    # )
+    # print("Parsing enhancers epigenomes")
+    # build(
+    #     bed_path="enhancers_regions.bed",
+    #     cell_lines=cell_lines_encode,
+    #     nan_threshold=1,
+    #     epigenomes_path="epigenomes",
+    #     targets_path="fantom_enhancers",
+    #     workers=4
+    # )
+
+    # promoters[["chrom","chromStart","chromEnd"]].to_csv(
+    #     "promoters_regions.bed",
+    #     sep="\t",
+    #     header=False,
+    #     index=False
+    # )
+
+    # print("Parsing promoters epigenomes")
+    # build(
+    #     bed_path="promoters_regions.bed",
+    #     cell_lines=cell_lines_encode,
+    #     nan_threshold=1,
+    #     epigenomes_path="epigenomes",
+    #     targets_path="fantom_promoters",
+    #     workers=4
+    # )
+
+    print("Downloading labels from ROADMAP")
+    enhancers, promoters = roadmap(
+        cell_lines = cell_lines_fantom, # List of cell lines to be considered.
+        window_size = 1000, # Window size to use for the various regions.
+        genome = "hg19", # Considered genome version. Currently supported only "hg19".
+        states = 18, # Number of the states of the model to consider. Currently supported only "15" and "18".
+        enhancers_labels = ("7_Enh", "9_EnhA1", "10_EnhA2"), # Labels to encode as active enhancers.
+        promoters_labels = ("1_TssA",), # Labels to enode as active promoters.
     )
 
-    enhancers.to_csv("enhancers.bed", sep="\t")
-    promoters.to_csv("promoters.bed", sep="\t")
+    enhancers.to_csv("roadmap_enhancers.bed", sep="\t")
+    promoters.to_csv("roadmap_promoters.bed", sep="\t")
 
     enhancers[["chrom","chromStart","chromEnd"]].to_csv(
-        "enhancers_regions.bed",
+        "roadmap_enhancers_regions.bed",
         sep="\t",
         header=False,
         index=False
     )
     print("Parsing enhancers epigenomes")
     build(
-        bed_path="enhancers_regions.bed",
+        bed_path="roadmap_enhancers_regions.bed",
         cell_lines=cell_lines_encode,
         nan_threshold=1,
         epigenomes_path="epigenomes",
-        targets_path="enhancers",
+        targets_path="roadmap_enhancers",
         workers=4
     )
 
     promoters[["chrom","chromStart","chromEnd"]].to_csv(
-        "promoters_regions.bed",
+        "roadmap_promoters_regions.bed",
         sep="\t",
         header=False,
         index=False
@@ -50,10 +96,10 @@ with Notipy():
 
     print("Parsing promoters epigenomes")
     build(
-        bed_path="promoters_regions.bed",
+        bed_path="roadmap_promoters_regions.bed",
         cell_lines=cell_lines_encode,
         nan_threshold=1,
         epigenomes_path="epigenomes",
-        targets_path="promoters",
+        targets_path="roadmap_promoters",
         workers=4
     )
