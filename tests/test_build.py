fea2de96d416c106a63cb06aa4ce1bec9aff8e54
LucaCappelletti94 <cappelletti.luca94@gmail.com>
2020-03-15 13:52:05 +0100

    Extended testing

--- tests/test_build.py
+++ tests/test_build.py
@@ -1,8 +1,10 @@
 import os
 from epigenomic_dataset import build
 import shutil
+import pytest
 
 def test_build():
     build("tests/test.bed", ["GM12892"])
-    build("tests/test.bed", ["GM12892"])
+    with pytest.warns(UserWarning): 
+        build("tests/test.bed", ["GM12892"])
     shutil.rmtree("targets")
