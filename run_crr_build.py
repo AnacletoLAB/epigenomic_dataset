9b6455db0a46e9cf4c31678b1654e52b7d5c2762
LucaCappelletti94 <cappelletti.luca94@gmail.com>
2020-12-02 16:42:54 +0100

    Increased number of extraction workers

--- run_crr_build.py
+++ run_crr_build.py
@@ -67,7 +67,7 @@ def run_pipeline(
             windows_size=windows_size,
             region=region
         ),
-        extraction_workers=4,
+        extraction_workers=12,
         concatenation_workers=20
     )
 
