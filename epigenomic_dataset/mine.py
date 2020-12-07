162d885ad9649937e2676628bfd191b280e27c99
LucaCappelletti94 <cappelletti.luca94@gmail.com>
2020-12-07 17:33:23 +0100

    Updated output paths

--- epigenomic_dataset/mine.py
+++ epigenomic_dataset/mine.py
@@ -61,15 +61,11 @@ def get_target_path(root: str, cell_line: str, assembly: str, assay_term_name: s
     -----------------------
     The path where to store the epigenomic data.
     """
-    file_name = "{}.csv.xz".format(
-        assay_term_name if target == "Unknown" else target
-    )
-    return "{root}/{cell_line}/{assay_term_name}.csv.xz".format(
+    return "{root}/{cell_line}/{file_name}.csv.xz".format(
         root=root,
         cell_line=cell_line,
         assembly=assembly,
-        assay_term_name=assay_term_name,
-        target=file_name
+        file_name=assay_term_name if target == "Unknown" else target
     )
 
 
