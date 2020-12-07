14509cc7a4bf516b60274e319c3338ba661b59b0
LucaCappelletti94 <cappelletti.luca94@gmail.com>
2020-12-07 16:14:53 +0100

    Fixed concatenation of features

--- epigenomic_dataset/concatenate.py
+++ epigenomic_dataset/concatenate.py
@@ -2,25 +2,22 @@ import pandas as pd
 import os
 from tqdm.auto import tqdm
 from typing import List
+from glob import glob
 from multiprocessing import Pool, cpu_count
 from .extract import load_epigenomes_table
 from .mine import get_target_path
 
 
-def to_dict(path: str, target: str):
+def to_dict(path: str):
+    feature_name = path.split(os.sep)[-1].split(".")[0]
     df = pd.read_csv(path, sep="\t", index_col=[0, 1, 2, 3]).round(2)
-    df.columns = pd.MultiIndex.from_product([[target], df.columns])
+    df.columns = pd.MultiIndex.from_product([[feature_name], df.columns])
     return df
-
-
-def _to_dict(kwargs):
-    return to_dict(**kwargs)
 
 
 def concatenate(
     root: str,
     cell_lines: List[str],
-    assembly: str,
     workers: int
 ):
     """Concatenate the targets into a single file.
@@ -32,33 +29,31 @@ def concatenate(
         to concatenate.
     cell_lines: List[str],
         Cell lines to consider.
-    assembly: str,
-        The genomic assembly of the data to be retrieved.
     workers: int,
         Workers to use to parallelize the loading.
     """
-    table = load_epigenomes_table(cell_lines, assembly)
     if workers == -1:
         workers = cpu_count()
     with Pool(min(cpu_count(), workers)) as p:
-        for cell_line, group in tqdm(table.groupby(["cell_line", "assay_term_name"]), leave=False, desc="Concatenating cell lines"):
-            path = "{root}/{assembly}/{cell_line}.csv.xz".format(
+        for cell_line in tqdm([
+            directory_name
+            for directory_name in os.listdir(root)
+            if os.path.isdir(directory_name)
+        ], leave=False, desc="Concatenating cell lines"):
+            path = "{root}/{cell_line}.csv.xz".format(
                 root=root,
-                assembly=assembly,
                 cell_line=cell_line
             )
+            paths = glob("{root}/{cell_line}/*.csv.xz".format(
+                root=root,
+                cell_line=cell_line
+            ))
+
             if os.path.exists(path):
                 continue
-            paths = [
-                {
-                    "path": get_target_path(root, cell_line, assembly, assay_term_name, target),
-                    "feature": assay_term_name if target == "Unknown" else target,
-                }
-                for assay_term_name, target in set(group[["assay_term_name", "target"]].values)
-            ]
 
             pd.concat(list(tqdm(
-                p.imap(_to_dict, paths),
+                p.imap(to_dict, path),
                 desc="Concatenating files",
                 total=len(paths),
                 leave=False
