960fcbec437979e9b4002110915199e449e9d066
LucaCappelletti94 <cappelletti.luca94@gmail.com>
2020-12-27 17:58:09 +0100

    Updated dependencies

--- setup.py
+++ setup.py
@@ -62,6 +62,7 @@ setup(
     install_requires=[
         "encodeproject>=1.0.19",
         "downloaders",
+        "pandas>=1.2.0",
         "pybwtool>=1.0.8",
         "crr_labels>=1.1.0",
         "notipy_me>=1.3.23"
