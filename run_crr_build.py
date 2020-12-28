2eabbe2e14f2c93473777ae4fda87a9f19f4dfa6
LucaCappelletti94 <cappelletti.luca94@gmail.com>
2020-12-28 09:48:29 +0100

    Restored set of window sizes

--- run_crr_build.py
+++ run_crr_build.py
@@ -81,7 +81,7 @@ if __name__ == "__main__":
     cell_lines_encode = cell_lines + ["MCF-7"]
     cell_lines_fantom = cell_lines + ["MCF7"]
     cell_lines_roadmap = ["A549", "GM12878", "H1", "HepG2", "K562"]
-    windows_sizes = (256,)  # (1024, 512, 256, 128, 64)
+    windows_sizes = (1024, 512, 256, 128, 64)
     assembly = "hg38"
     # We are not computing RoadMap right now
     # because we are still choosing the states from the model to be used.
