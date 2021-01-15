f99ef737e08fa69dda0d39a956a9d6989e5eae3e
LucaCappelletti94 <cappelletti.luca94@gmail.com>
2021-01-15 15:57:30 +0100

    Published new version introducing normalization

---
+++ epigenomic_dataset/utils/__init__.py
@@ -0,0 +1,6 @@
+"""Generic utilities used in the package."""
+from .normalize_epigenomic_data import normalize_epigenomic_data
+
+__all__ = [
+    "normalize_epigenomic_data",
+]
---
+++ epigenomic_dataset/utils/normalize_epigenomic_data.py
@@ -0,0 +1,42 @@
+"""Module providing normalization for epigenomic data."""
+from typing import Tuple
+import numpy as np
+from sklearn.impute import KNNImputer
+from sklearn.preprocessing import StandardScaler
+
+
+def normalize_epigenomic_data(
+    train_x: np.ndarray,
+    test_x: np.ndarray
+) -> Tuple[np.ndarray]:
+    """Return imputed and normalized epigenomic data.
+
+    We fit the imputation and normalization on the training data and
+    apply it to both the training data and the test data.
+
+    Parameters
+    -------------------------
+    train_x: np.ndarray,
+        Training data to use to fit the imputer and scaled.
+    test_x: np.ndarray,
+        Test data to be normalized.
+
+    Returns
+    -------------------------
+    Tuple with imputed and scaled train and test data.
+    """
+    # Create the imputer and scaler object
+    imputer = KNNImputer()
+    scaler = StandardScaler()
+    # Fit the imputer object
+    imputer.fit(train_x)
+    # Impute the train and test data
+    imputed_train_x = imputer.transform(train_x)
+    imputed_test_x = imputer.transform(test_x)
+    # Fit the scaler object
+    scaler.fit(imputed_train_x)
+    # Scale the train and test data
+    scaled_train_x = scaler.transform(imputed_train_x)
+    scaled_test_x = scaler.transform(imputed_test_x)
+    # Return the normalized data
+    return scaled_train_x, scaled_test_x
