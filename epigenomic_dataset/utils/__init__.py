6884ded6ee8c4ae4bd18c3647119a5b0d7bc99cb
LucaCappelletti94 <cappelletti.luca94@gmail.com>
2021-06-15 11:03:49 +0200

    Added method that returns list of available window sizes

--- epigenomic_dataset/utils/__init__.py
+++ epigenomic_dataset/utils/__init__.py
@@ -1,8 +1,10 @@
 """Generic utilities used in the package."""
 from .normalize_epigenomic_data import normalize_epigenomic_data
 from .cell_lines import get_cell_lines
+from .window_sizes import get_window_sizes
 
 __all__ = [
     "normalize_epigenomic_data",
-    "get_cell_lines"
+    "get_cell_lines",
+    "get_window_sizes"
 ]
